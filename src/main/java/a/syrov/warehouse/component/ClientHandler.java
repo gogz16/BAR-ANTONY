f0960425863eccfa6ea746eb49cfcfb4f16fec6c
a.syrov <antonbiba@yandex.ru>
2025-10-16 13:46:07 +0300

    add http request

--- src/main/java/a/syrov/warehouse/component/ClientHandler.java
+++ src/main/java/a/syrov/warehouse/component/ClientHandler.java
@@ -18,12 +18,28 @@ public class ClientHandler implements Runnable {
     public void run() {
         boolean exit = false;
         try (InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream()) {
+             OutputStream outputStream = socket.getOutputStream()) {
+
             PrintWriter printWriter = new PrintWriter(outputStream, true);
-            printWriter.println("Hello from server! Type stock to see all stock in Database");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String firstLine = bufferedReader.readLine();
+            if (firstLine == null) return;
+
+            if (firstLine.startsWith("GET")) {
+                String httpAnswer = "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/html; charset=UTF-8\r\n" +
+                        "\r\n" +
+                        "<h1>Бар 'У Энтони' работает!</h1>";
+                printWriter.println(httpAnswer);
+                printWriter.flush();
+            }
+            //Блок с telnet не смог прикрутить так, чтобы работал одновременно с http в браузере, поэтому telnet закомментирую дальше
+           /* printWriter.println("Hello from server! Type stock to see all stock in Database");
+            printWriter.flush();
+
             while (!exit) {
-                String request = bufferedReader.readLine();
+                String request = firstLine;
                 switch (request) {
                     case "stock":
                         StockDAO stockDAO = new StockDAO();
@@ -35,7 +51,12 @@ public class ClientHandler implements Runnable {
                         exit = true;
                         break;
                 }
-            }
+                if (!exit) {
+                    request = bufferedReader.readLine();
+                    if (request == null) break; // Клиент закрыл соединение
+                }
+
+            }*/
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
